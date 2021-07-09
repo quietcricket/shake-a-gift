693be1d44ba1a8743ab27a2050225eef4baef890
skyLevi14 <levsgutierrez@gmail.com>
2021-07-09 17:34:42 +0800

    implement image change when shaked

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 let gameImg2 = "background-image: url('img/bg-img-2.jpg')";
 let gameImg3 = "background-image: url('img/bg-img-3.jpg')";
-
+let gameBG = document.querySelector(".section-game");
 
 class ShakeAGift {
 
@@ -58,22 +58,26 @@ class ShakeAGift {
   _updateGame() {
     // reference self via window.game instance
     let t = (new Date().getTime() - game.startTime);
+    
     // Show Ready Go
     let seconds = Math.floor(t / 1000);
     if (seconds >= 2) {
       // update the circle
       document.querySelector("svg.circle circle").style.strokeDashoffset = (t - 2000) / CONFIG.GAME_DURATION / 2;
     }
+
     //Game end
     if (seconds >= CONFIG.GAME_DURATION + 2) {
       game.endGame();
       return;
     }
+
     requestAnimationFrame(game._updateGame);
+    
     if (seconds != game.currentTime) {
       game.utils.playSound('tik');
       let ele = document.querySelector(".timer");
-      let gameBG = document.querySelector(".section-game");
+     
       if (seconds == 0) {
         ele.innerHTML = "よーい、ドン！";
         ele.classList.add("timer-small");
@@ -83,7 +87,7 @@ class ShakeAGift {
         ele.innerHTML = "GO";
       } else if (seconds == 2) {
         //TODO: swapping the background here
-        gameBG.setAttribute("style", gameImg3);
+       
         ele.classList.remove("timer-small");
         game.prevMotion = undefined;
         game.shakeTime = new Date().getTime();
@@ -92,6 +96,7 @@ class ShakeAGift {
       } else {
         ele.innerHTML = CONFIG.GAME_DURATION + 2 - seconds;
       }
+
       ele.style.animationName = "";
       void ele.offsetWidth;
       ele.style.animationName = "zoomin";
@@ -102,7 +107,7 @@ class ShakeAGift {
    * Share result as a tweet
    */
   share() {
-    let message = this.shakeCount >= CONFIG[''] ? CONFIG.SUCCESS_TWEET : CONFIG.FAILURE_TWEET;
+    let message = this.shakeCount >= CONFIG.PRIZE_UNLOCK ? CONFIG.SUCCESS_TWEET : CONFIG.FAILURE_TWEET;
     message = message.replace('[[SHAKE_COUNT]]', this.shakeCount) + '\n' + document.location.href;
     document.location.href = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(message);
     //console.log(message);
@@ -148,10 +153,14 @@ class ShakeAGift {
     game.totalMotion.z += Math.abs(currMotion.z - game.prevMotion.z);
     game.prevMotion = { x: currMotion.x, y: currMotion.y, z: currMotion.z };
     let t = new Date().getTime();
+    
     if (t - game.shakeTime >= CONFIG.SHAKE_INTERVAL) {
       game.shakeTime = t;
       if (game.totalMotion.x + game.totalMotion.y + game.totalMotion.z > CONFIG.SHAKE_THRESHOLD * 3) {
         game.shakeCount++;
+        if(game.shakeCount == 1){
+          gameBG.setAttribute("style", gameImg3);
+        }
         document.querySelector(".section-game .shake-count").innerHTML = game.shakeCount;
       }
       game.totalMotion = { x: 0, y: 0, z: 0 };
@@ -186,7 +195,7 @@ class Utils {
       if (this.supportsWebp) {
         ele.src = ele.src.replace("gif", "webp");
       }
-      ele.style.left = (document.querySelector(".section").offsetWidth - parseInt(ele.getAttribute("img-width"))) / 2 + "px";
+      ele.style.left = (document.querySelector(".section").offsetWidth - parseInt(ele.getAttribute("img-width"))) / 3.2 + "px";
     });
   }
 
@@ -227,5 +236,5 @@ class Utils {
 }
 window.game = new ShakeAGift();
 // game.startGame();
-//game.show("result-pass");
+// game.show("result-pass");
 game.show("landing");
